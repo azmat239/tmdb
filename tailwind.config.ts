95ece5e7370b5b4ad01e63266b4a16e0bd83139f
Azmat <azmat239@gmail.com>
2023-09-26 10:42:05 +0530

    add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,19 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        darkBlue :' #032541',
+        darkGrey : '#757575',
+        purple : '#805BE7',
+        lightBlue : '#01B4E4',
+        lightBrown : '#897474',
+        grey : '#D7D7D7',
+        lightGrey: '#E4E4E4',
+      }
     },
   },
   plugins: [],
 }
 export default config
+
+
