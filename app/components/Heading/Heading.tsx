2c6788a6377cd4a9bf485c91003b3f3a48f192a1
Azmat <azmat239@gmail.com>
2023-09-26 15:08:42 +0530

    add dynamic tag

--- app/components/Heading/Heading.tsx
+++ app/components/Heading/Heading.tsx
@@ -1,9 +1,15 @@
 import classNames from "classnames";
 import React from "react";
 import { HeadingType } from "./Heading.types";
+import { FC } from "react";
 
-const Heading = ({ text, variant, extraClasses }: HeadingType) => {
-  let tag1 = classNames({
+const Heading: FC<HeadingType> = ({
+  text,
+  variant,
+  extraClasses,
+  as: Tag = "h5",
+}) => {
+  let cls = classNames({
     "text-[48px] leading-[48px]": variant == "48",
     "text-[36px] leading-[44px]": variant == "36",
     "text-[32px] leading-[40px]": variant == "32",
@@ -11,7 +17,8 @@ const Heading = ({ text, variant, extraClasses }: HeadingType) => {
     "text-[22px] leading-[28px]": variant == "22",
     "text-[20px] leading-[24px]": variant == "20",
   });
-  return <h1 className={`${tag1} ${extraClasses}`}>{text} </h1>;
+
+  return <Tag className={`${cls} ${extraClasses}`}>{text} </Tag>;
 };
 
 export default Heading;
--- app/components/Heading/Heading.types.ts
+++ app/components/Heading/Heading.types.ts
@@ -1,4 +1,6 @@
+import { ElementType } from "react";
 export type HeadingType = {
+  as?: ElementType;
   text: string;
   variant: "20" | "22" | "24" | "32" | "36" | "48";
   extraClasses?: string;
